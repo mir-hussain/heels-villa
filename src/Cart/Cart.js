eaf4a68ebb80808fa8a2f3562172e6e3bddd1406
mir-hussain <mirhussainmurtaza@gmail.com>
2022-03-28 16:26:07 +0600

    cart added

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,13 +1,20 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = ({ cart }) => {
   return (
-    <div>
+    <div className='cart'>
       <h1>Order Summery</h1>
       {cart.map((product) => (
-        <p>
-          {product.name} {product.color}
-        </p>
+        <div className='cart-item'>
+          <img src={product.pairImage} alt='' />
+          <div>
+            <p>
+              {product.name} {product.color}
+            </p>
+            <p>{product.price}</p>
+          </div>
+        </div>
       ))}
     </div>
   );
