cc67be6b093142b0f7895c0d9981d5d24f95ebeb
mir-hussain <mirhussainmurtaza@gmail.com>
2022-03-28 16:40:02 +0600

    offer button added

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
+  const [offer, setOffer] = useState(false);
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      setOffer(true);
+    }
+  }, [cart.length]);
+
+  const handleOffer = () => {
+    console.log("offer");
+  };
+
   return (
     <div className='cart'>
       <h1>Order Summery</h1>
-      {cart.map((product) => (
-        <div className='cart-item'>
+      {cart.map((product, index) => (
+        <div key={index} className='cart-item'>
           <img src={product.pairImage} alt='' />
           <div>
             <p>
@@ -16,6 +28,14 @@ const Cart = ({ cart }) => {
           </div>
         </div>
       ))}
+      <p>Buy one get one free</p>
+      <button
+        className={offer ? "offer-button" : "offer-button-disabled"}
+        onClick={handleOffer}
+        disabled={!offer}
+      >
+        Get one for me
+      </button>
     </div>
   );
 };
